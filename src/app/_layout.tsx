6a1fca1650e35658effd881f600498425c56dd2b
Echo AI <echo@expo.dev>
2026-02-06 02:09:32 +0000

    add a header menu button

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Stack from "expo-router/stack";
 import * as AC from "@bacons/apple-colors";
 import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
+import { Image } from "expo-image";
+import { Alert, Pressable } from "react-native";
 
 const stackPreset: NativeStackNavigationOptions =
   process.env.EXPO_OS !== "ios"
@@ -15,11 +17,31 @@ const stackPreset: NativeStackNavigationOptions =
         headerBackButtonDisplayMode: "default",
       };
 
+function HeaderMenuButton() {
+  return (
+    <Pressable
+      onPress={() => Alert.alert("Menu", "You tapped the menu button!")}
+      hitSlop={8}
+    >
+      <Image
+        source="sf:ellipsis.circle"
+        style={{ width: 22, height: 22, color: AC.systemBlue as any }}
+      />
+    </Pressable>
+  );
+}
+
 export default function Layout() {
   return (
     <ThemeProvider>
       <Stack screenOptions={stackPreset}>
-        <Stack.Screen name="index" options={{ title: "Hello World" }} />
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Hello World",
+            headerRight: () => <HeaderMenuButton />,
+          }}
+        />
       </Stack>
     </ThemeProvider>
   );
